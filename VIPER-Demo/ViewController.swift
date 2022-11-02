51b6564b8d3dbb991496df9637d1c52a9d0cf3ff
Auropriya Sinha <auropriyasinha241995@gmail.com>
2022-11-02 12:19:39 +0530

    Initial Commit

---
+++ VIPER-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER-Demo
+//
+//  Created by Auropriya Sinha on 02/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
